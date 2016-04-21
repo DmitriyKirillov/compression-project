c253bac00b55dd530901db9fa547f3a0801a65fe
Dmitry Kirillov <kirilltheterrible@gmail.com>
2016-04-22 02:34:10 +0300

    Working version of Huffman. Not covered by tests

--- codecs/library/Huffman/Huffman.h
+++ codecs/library/Huffman/Huffman.h
@@ -3,6 +3,7 @@
 #include <library/common/codec.h>
 #include <algorithm>
 #include <bitset>
+#include <functional>
 #include <map>
 #include <math.h>
 #include <iostream>
@@ -13,42 +14,55 @@ namespace Codecs {
     class BinString {
     private:
         string data;
-        unsigned short unused_bits = 0;
+        unsigned short unused_bits;
+
     public:
         void push_back(bool value) {
             if (unused_bits > 0) {
                 if (value) {
-                    unsigned char add = std::pow(2, unused_bits - 1);
+                    unsigned char add = 1;
+                    add <<= (unused_bits - 1);
                     unsigned char val = add | static_cast<unsigned char>(data[data.size() - 1]);
                     data[data.size() - 1] = static_cast<char>(val);
                 }
                 --unused_bits;
             } else {
-                unsigned char add = (value) ? (std::pow(2, 7)) : (0);
+                unsigned char add = (value) ? (static_cast<unsigned char>(1)) : (static_cast<unsigned char>(0));
+                add <<= 7;
                 data.push_back(static_cast<char>(add));
                 unused_bits = 7;
             }
         }
 
-        void push_back(const vector<bool> &values) {
-            for (bool elem : values) {
-                push_back(elem);
+        void extend(const vector<bool> &values) {
+            data.reserve(values.size() / 8 + 1 + data.size());
+            size_t pos = 0;
+            while (unused_bits && pos < values.size()) {
+                push_back(values[pos]);
+                ++pos;
             }
+            for (; pos + 8 <= values.size() && !unused_bits; pos += 8) {
+                unsigned char out = 0;
+                for (int i = 0; i < 8; ++i) {
+                    out <<= 1;
+                    out |= (values[pos + i]) ? (1) : (0);
+                }
+                data.push_back(out);
+            }
+            for (; pos < values.size(); ++pos) {
+                push_back(values[pos]);
+            }
+        }
+
+        void push_back(unsigned char value) {
+            unsigned char last_b = static_cast<unsigned char>(data[data.size() - 1]);
+            unsigned char add = value >> (8 - unused_bits);
+            data[data.size() - 1] = static_cast<char>(last_b | add);
+            data.push_back(static_cast<char>(value << unused_bits));
         }
 
         void push_back(char value) {
-            unsigned char val = static_cast<unsigned char>(value);
-            unsigned char last_b = static_cast<unsigned char>(data[data.size() - 1]);
-            unsigned char add = val >> (8 - unused_bits);
-            data[data.size() - 1] = static_cast<char>(last_b | add);
-            data.push_back(static_cast<char>(val << unused_bits));
-        }
-
-        template<size_t N>
-        void push_back(const std::bitset<N> &values) {
-            for (size_t i = 0; i < N; ++i) {
-                push_back(values[i]);
-            }
+            push_back(static_cast<unsigned char>(value));
         }
 
         string read(size_t n) const {
@@ -69,17 +83,32 @@ namespace Codecs {
             return data;
         }
 
-        BinString() : data() { }
+        BinString() : data(), unused_bits(0) { }
 
         BinString(const vector<bool> &init) {
+            unused_bits = 0;
             for (bool elem : init) {
                 push_back(elem);
             }
         }
 
-        BinString(const string &init) : data(init) { }
+        BinString(const string &init) : data(init), unused_bits(0) { }
 
-        void debug_print() {
+        BinString(std::string &&s) {
+            unused_bits = 0;
+            data = std::move(s);
+        }
+
+        BinString(BinString &&s) {
+            unused_bits = s.unused_bits;
+            data = std::move(s.data);
+        }
+
+        typename std::remove_reference<std::string>::type&& move() {
+            return std::move(data);
+        }
+
+        void debug_print() const {
             for (char elem : data) {
                 std::bitset<8> out(elem);
                 std::cout << out << ' ';
@@ -97,125 +126,171 @@ namespace Codecs {
             unsigned char leaf_value;
             bool is_escape;
         };
-        const unsigned MAX_CODE_L = 7;
+        const unsigned MAX_CODE_L = 9;
+        const unsigned BITS_PER_SYMBOL_IN_DICT = 5;
     private:
-        std::map<char, unsigned> codeLenths;
-        std::vector<node> code_tree;
-        std::map<char, std::vector<bool>> precounted;
-        std::vector<bool> escape_code;
+        vector<unsigned> codeLenths;
+        vector<node> code_tree;
+        node tree_root;
+        vector<vector<bool>> precounted;
+        vector<bool> escape_code;
 
-        template<typename T>
-        void InplaceElements(vector<node> &tree, T &It, T end, size_t p,
-                             unsigned target_level, unsigned level) {
-            if (It == end || level > target_level) {
+        void InplaceSymbols(vector<node> &tree, size_t v, const vector<unsigned char> &chars_list,
+                size_t &p, size_t target_level, size_t level = 0) {
+            if (p >= chars_list.size())
+                return;
+            if (level == target_level && tree[v].is_leaf) {
+                tree[v].left = 0;
+                tree[v].right = 0;
+                tree[v].leaf_value = chars_list[p];
+                ++p;
+            } else if (level < target_level) {
+                tree[v].is_leaf = false;
+                InplaceSymbols(tree, tree[v].left, chars_list, p, target_level, level + 1);
+                InplaceSymbols(tree, tree[v].right, chars_list, p, target_level, level + 1);
+            }
+        }
+
+        void CountLenths(const vector<node> &tree, std::vector<unsigned> &buffer, size_t p, unsigned layer = 0) {
+            if (layer > MAX_CODE_L) {
                 return;
             }
-            if (level == target_level && tree[p].is_leaf) {
-                tree[p].leaf_value = *It;
-                ++It;
+            if (tree[p].is_leaf) {
+                buffer[tree[p].leaf_value] = layer;
             } else {
-                tree[p].is_leaf = false;
+                CountLenths(tree, buffer, tree[p].left, layer + 1);
+                CountLenths(tree, buffer, tree[p].right, layer + 1);
             }
-            InplaceElements(tree, It, end, tree[p].left, target_level, level + 1);
-            InplaceElements(tree, It, end, tree[p].right, target_level, level + 1);
         }
 
-        void InplaceElements(vector<node> &tree, size_t p, unsigned target_level, unsigned level) {
-            while (level < target_level) {
-                p = tree[p].left;
-                tree[p].is_leaf = false;
-                ++level;
-            }
-            tree[p].is_escape = true;
-        }
-
-    public:
         void MakeCodeTree() {
             unsigned m = 0;
-            std::map<unsigned, vector<char>> lenths;
-            for (std::pair<char, unsigned> elem : codeLenths) {
-                m = std::max(m, elem.second);
-                if (lenths.find(elem.second) != lenths.end()) {
-                    lenths[elem.second].push_back(elem.first);
-                } else {
-                    lenths[elem.second] = {elem.first};
+            for (unsigned len : codeLenths) {
+                m = std::max(m, len);
+            }
+            vector<vector<unsigned char>> chars_by_layer(m + 1, vector<unsigned char>());
+            for (size_t i = 0; i < codeLenths.size(); ++i) {
+                if (codeLenths[i]) {
+                    chars_by_layer[codeLenths[i]].push_back(static_cast<unsigned char>(i));
                 }
             }
-            for (auto It = lenths.begin(); It != lenths.end(); ++It) {
-                std::sort(It->second.begin(), It->second.end());
+
+            size_t max_layer = chars_by_layer.size() - 1;
+            if (chars_by_layer[max_layer].size() == static_cast<size_t>(std::pow(2, max_layer))) {
+                ++max_layer;
             }
 
-            unsigned total_m = (std::pow(2, m) > lenths[m].size()) ? (m) : (m + 1);
-
-            code_tree.resize(std::pow(2, total_m + 1));
+            code_tree.resize(static_cast<size_t>(std::pow(2, max_layer + 1)));
             for (size_t i = 1; i < code_tree.size(); ++i) {
                 code_tree[i].left = 2 * i;
                 code_tree[i].right = 2 * i + 1;
                 code_tree[i].is_leaf = true;
                 code_tree[i].is_escape = false;
+                code_tree[i].leaf_value = 0;
             }
 
-            InplaceElements(code_tree, 1, total_m, 0);
-
-            for (unsigned d = m; d > 0; --d) {
-                if (lenths.find(d) != lenths.end()) {
-                    auto It = lenths[d].begin();
-                    InplaceElements(code_tree, It, lenths[d].end(), 1, d, 0);
-                }
+            size_t p = 1;
+            for (unsigned level = 0; level < max_layer; ++level) {
+                code_tree[p].is_leaf = false;
+                p = code_tree[p].left;
             }
-            /*for (node elem : code_tree) {
-                std::cout << elem.left << ' ' << elem.right << ' ' << elem.leaf_value << ' ' << elem.is_escape << '\n';
-            }*/
+            code_tree[p].is_escape = true;
+
+            for (size_t j = chars_by_layer.size() - 1; j > 0; --j) {
+                size_t next_char_to_place = 0;
+                InplaceSymbols(code_tree, 1, chars_by_layer[j], next_char_to_place, j);
+            }
+            tree_root = code_tree[1];
         }
 
-        void DFS(const std::vector<node> &tree, size_t v, std::map<char, std::vector<bool>> &values,
-                 std::vector<bool> &escape_code, std::vector<bool> prefix) {
-            if (tree[v].is_leaf) {
-                values[tree[v].leaf_value] = prefix;
-                return;
-            }
-            if (tree[v].is_escape) {
+        void MakeCodes(const vector<node> &tree, size_t pos, vector<bool> prefix,
+                       vector<vector<bool>> &out, vector<bool> &escape_code) {
+            if (tree[pos].is_escape) {
                 escape_code = prefix;
-                return;
+            } else if (tree[pos].is_leaf) {
+                out[tree[pos].leaf_value] = prefix;
+            } else {
+                vector<bool> upd = prefix;
+                upd.push_back(false);
+                MakeCodes(tree, tree[pos].left, upd, out, escape_code);
+                *(upd.end() - 1) = true;
+                MakeCodes(tree, tree[pos].right, upd, out, escape_code);
             }
-
-            std::vector<bool> left_prefix = prefix;
-            left_prefix.push_back(false);
-            std::vector<bool> right_prefix = prefix;
-            right_prefix.push_back(true);
-            DFS(tree, tree[v].left, values, escape_code, left_prefix);
-            DFS(tree, tree[v].right, values, escape_code, right_prefix);
         }
 
         void MakeCodes() {
-            if (!precounted.empty()) {
-                DFS(code_tree, 1, precounted, escape_code, std::vector<bool>());
-            }
+            precounted.resize(256);
+            MakeCodes(code_tree, 1, {}, precounted, escape_code);
         }
-
+    public:
         void encode(string &encoded, const string_view &raw) const override {
             BinString enc;
-            for (char symbol : raw) {
-                if (precounted.find(symbol) != precounted.end()) {
-                    enc.push_back(precounted.at(symbol));
+            for (char c : raw) {
+                unsigned char symbol = static_cast<unsigned char>(c);
+                if (precounted[symbol].size()) {
+                    enc.extend(precounted[symbol]);
                 } else {
-                    enc.push_back(escape_code);
+                    enc.extend(escape_code);
                     enc.push_back(symbol);
                 }
             }
-            encoded = enc.read();
+            encoded = enc.move();
         }
 
         void decode(string &raw, const string_view &encoded) const override {
-            raw.assign(encoded.begin(), encoded.end());
+            node current_node = tree_root;
+            bool code[8];
+            vector<bool> escape_buffer;
+            bool to_escape = false;
+            for (auto It = encoded.begin(); It != encoded.end(); ++It) {
+                unsigned char val = static_cast<unsigned char>(*It);
+                for (int i = 7; i >= 0; --i) {
+                    code[i] = val % 2 != 0;
+                    val >>= 1;
+                }
+                for (int i = 0; i < 8; ++i) {
+                    if (to_escape) {
+                        if (escape_buffer.size() == 8) {
+                            unsigned char out = 0;
+                            for (int j = 0; j < 8; ++j) {
+                                out <<= 1;
+                                out |= escape_buffer[j];
+                            }
+                            raw.push_back(static_cast<char>(out));
+                            to_escape = false;
+                            escape_buffer.resize(0);
+                            --i;
+                        } else {
+                            escape_buffer.push_back(code[i]);
+                        }
+                    } else {
+                        current_node = code_tree[(code[i]) ? (current_node.right) : (current_node.left)];
+                        if (current_node.is_escape) {
+                            to_escape = true;
+                            current_node = tree_root;
+                        } else if (current_node.is_leaf) {
+                            raw.push_back(current_node.leaf_value);
+                            current_node = tree_root;
+                        }
+                    }
+                }
+
+            }
         }
 
         string save() const override {
             BinString dict;
-            for (auto It = codeLenths.begin(); It != codeLenths.end(); ++It) {
-                dict.push_back(It->first);
-                std::bitset<3> len(It->second);
-                dict.push_back(len);
+            vector<bool> buffer(BITS_PER_SYMBOL_IN_DICT);
+            for (size_t i = 0; i < codeLenths.size(); ++i) {
+                if (codeLenths[i]) {
+                    dict.push_back(static_cast<unsigned char>(i));
+                    unsigned val = codeLenths[i];
+                    for (unsigned j = 0; j < buffer.size(); ++j) {
+                        buffer[i] = val % 2;
+                        val >>= 1;
+                    }
+                    dict.extend(buffer);
+                }
             }
             return dict.read();
         }
@@ -226,31 +301,19 @@ namespace Codecs {
             return 10000;
         };
 
-        void CountLenths(const vector<node> &v, std::map<char, unsigned> &buffer, size_t p, unsigned layer) {
-            if (layer > MAX_CODE_L) {
-                return;
-            }
-            if (v[p].is_leaf) {
-                buffer[v[p].leaf_value] = layer;
-            } else {
-                CountLenths(v, buffer, v[p].left, layer + 1);
-                CountLenths(v, buffer, v[p].right, layer + 1);
-            }
-        }
-
         void learn(const StringViewVector &samples) override {
-            vector<unsigned long> frequencies(256);
+            vector<unsigned long long> frequencies(256, 0);
             for (auto It = samples.begin(); It != samples.end(); ++It) {
                 for (auto It_s = (*It).begin(); It_s != (*It).end(); ++It_s) {
-                    frequencies.at(*It_s) += 1;
+                    unsigned char symbol = static_cast<unsigned char>(*It_s);
+                    frequencies[symbol] += 1;
                 }
             }
 
-
             vector<node> tree;
-            std::priority_queue<std::pair<unsigned long, size_t>,
-                    vector<std::pair<unsigned long, size_t>>,
-                    std::greater<std::pair<unsigned long, size_t>>> q;
+            std::priority_queue<std::pair<unsigned long long, size_t>,
+                    vector<std::pair<unsigned long long, size_t>>,
+                    std::greater<std::pair<unsigned long long, size_t>>> q;
             for (unsigned i = 0; i < 256; ++i) {
                 if (frequencies[i]) {
                     tree.push_back({0, 0, true, static_cast<unsigned char>(i), false});
@@ -269,9 +332,9 @@ namespace Codecs {
                 q.push({first.first + first.second, tree.size() - 1});
             }
 
-            codeLenths.clear();
+            codeLenths = std::vector<unsigned> (256, 0);
 
-            CountLenths(tree, codeLenths, tree.size() - 1, 0);
+            CountLenths(tree, codeLenths, tree.size() - 1);
             MakeCodeTree();
             MakeCodes();
         }
@@ -279,4 +342,4 @@ namespace Codecs {
         void reset() override { }
     };
 
-}
+} //  namespace Huffman
--- codecs/library/Huffman/test/test.cpp
+++ codecs/library/Huffman/test/test.cpp
@@ -1,6 +1,7 @@
 #include <library/Huffman/Huffman.h>
-#include <bitset>
+#include <experimental/string_view>
 #include <string>
+#include <vector>
 // #include <library/tests_common/tests_common.h>
 
 /*TEST(HuffmanCodecTest, Works) {
@@ -17,8 +18,25 @@ int main(int argc, char **argv) {
 int main() {
     Codecs::HuffmanCodec codec;
 
-    codec.learn({"Hello, World. We can construct a model now"});
+    codec.learn(
+            {"Lorem ipsum dolor sit amet, consectetur adipisicing elit, "
+                     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                     "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"
+                     "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in"
+                     "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat"
+                     "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."});
 
-    std::cout << codec.save() << '\n';
+    std::string sample_raw = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris";
+
+    std::string code;
+    codec.encode(code, sample_raw);
+    std::string decoded;
+    codec.decode(decoded, code);
+    std::cout << ((decoded == sample_raw) ? ("Matched") : ("Didn't matched")) << '\n' << sample_raw
+    << "\nCompression ratio: " << static_cast<double>(sample_raw.size()) / static_cast<double>(code.size())
+    << '\n' << decoded;
+
+
+
     return 0;
 }
